6cf15fcf9adc85cb39b8fb28c4bcbe435125f2a0
mrkvv <markoffleonid@mail.ru>
2026-04-19 11:23:12 +0300

    feat(backend): NewsHandlerService базовая структура

---
+++ backend/NewsHandlerService/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+	java
+	id("org.springframework.boot") version "4.0.5"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "scrollic"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-cassandra-reactive")
+	implementation("org.springframework.boot:spring-boot-starter-kafka")
+	testImplementation("org.springframework.boot:spring-boot-starter-data-cassandra-reactive-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-kafka-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
