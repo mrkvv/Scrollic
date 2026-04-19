a93d6aa27070391b914df6f3d76746daaab7b889
mrkvv <markoffleonid@mail.ru>
2026-04-19 22:23:37 +0300

    revert(NewsFetcher): Убрал Actuator
    Задумка поменялась, он тут более не нужен. План такой: NewsFetcher будет ждать, пока запустится NewsHandler, и только потом начнет производить записи в Kafka. То есть теперь healthcheck не нужен для NewsFetcher, а нужен для NewsHandler.

--- backend/NewsFetcherService/build.gradle.kts
+++ backend/NewsFetcherService/build.gradle.kts
@@ -21,9 +21,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-kafka")
 	testImplementation("org.springframework.boot:spring-boot-starter-kafka-test")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
-
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
