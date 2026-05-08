5995e0630b70e66116279752f8502ebfd533cbee
mrkvv <markoffleonid@mail.ru>
2026-05-08 14:13:52 +0300

    fix(NewsHandler): корректная отправка acknowledgment в Kafka
    До этого при перезапуске NewsHandler'а он заново обрабатывал все сообщения топика, теперь он корректно отсылает уведомление, что сообщение было обработано

--- backend/NewsHandlerService/src/main/java/scrollic/news_handler_service/consumer/NewsConsumer.java
+++ backend/NewsHandlerService/src/main/java/scrollic/news_handler_service/consumer/NewsConsumer.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
-import reactor.core.scheduler.Schedulers;
 import scrollic.news_handler_service.dto.NewsArticle;
 import scrollic.news_handler_service.service.NewsProcessorService;
 
@@ -24,18 +23,14 @@ public class NewsConsumer {
     public void consume(NewsArticle article, Acknowledgment acknowledgment) {
         LOGGER.info("Получена новость: {}", article.getUrl());
 
-        newsProcessorService.processAndSave(article)
-                .publishOn(Schedulers.boundedElastic())
-                .subscribe(
-                        v -> {
-                            LOGGER.info("Новость {} успешно обработана", article.getUrl());
-                            acknowledgment.acknowledge();
-                        },
-                        error -> {
-                            LOGGER.error("Ошибка обработки новости {}: {}",
-                                    article.getUrl(), error.getMessage());
-                        }
-                );
+        try {
+            newsProcessorService.processAndSave(article).block();
+            LOGGER.info("Новость {} успешно обработана", article.getUrl());
+            acknowledgment.acknowledge();
+        }
+        catch (Exception e) {
+            LOGGER.error("Ошибка обработки новости {}: {}", article.getUrl(), e.getMessage());
+        }
     }
 
 }
